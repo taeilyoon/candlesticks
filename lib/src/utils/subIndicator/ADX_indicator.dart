5dd0c38dfc41490d7dfcd57c3116e8f76c694278
yuntaeil <zkrxpdlf0808@gmail.com>
2022-11-22 21:26:57 +0900

    ADX

--- lib/src/utils/subIndicator/ADX_indicator.dart
+++ lib/src/utils/subIndicator/ADX_indicator.dart
@@ -6,43 +6,52 @@ import 'package:candlesticks/src/models/subIndicator_chart_type.dart';
 import 'package:flutter/material.dart';
 
 import '../../models/sub_indicator.dart';
+import 'dart:math';
 
 class ADXIndicatorIndicator extends SubIndicator {
   ADXIndicatorIndicator({
-    int periods = 0,
+    int periods = 1,
     required Color color,
     String? label,
   }) : super(
-            chartStyle: SubIndicatorChartType.line,
-            name: label ?? "ADC ${periods}",
-            dependsOnNPrevCandles: periods * 2,
-            calculator: (index, candles) {
-              return [candles[index].volume];
-            },
-            calculatorWithStyle: (index, candles) {
-              if (index == candles.length - 1) {
-                return [
-                  ColorWithCalculatorValue()
-                    ..value = candles[index].volume
-                    ..color = color
-                ];
-              }
-              return [
-                ColorWithCalculatorValue()
-                  ..value = candles[index].volume
-                  ..color = candles[index + 1].volume > candles[index].volume
-                      ? Colors.red
-                      : Colors.blue
-              ];
-            },
-            max: (i, c, c2) {
-              return c2.map((e) => e.volume).toList().max();
-            },
-            min: (i, c, _) {
-              return 0;
-            }
-            // indicatorComponentsStyles: [
-            //   IndicatorStyle(name: "cci", bullColor: color),
-            // ]
-            );
+      chartStyle: SubIndicatorChartType.line,
+      name: label ?? "ADC ${periods}",
+      dependsOnNPrevCandles: periods * 2,
+      calculator: (index, candles) {
+        return [candles[index].volume];
+      },
+      calculatorWithStyle: (index, candles) {
+        var trueRange = max(
+            max(
+                candles[index].high - candles[index].low,
+                (candles[index].high - nz(candles[index + 1].close))
+                    .abs()),
+            (candles[index].low - nz(candles[index + 1].close)).abs());
+
+        var directionalMovementPlus = candles[index].high -
+            nz(candles[index + 1].high) >
+            nz(candles[index + 1].low) - candles[index].low ? max(
+            candles[index].high - nz(candles[index + 1].high), 0) : 0;
+        var directionalMovementMinus = nz(candles[index+1].low) - candles[index].low > candles[index].high - nz(candles[index+1].high)
+            ? max(nz(candles[index+1].low) - candles[index].low, 0)
+            : 0;
+
+
+
+
+        var plusm = candles[index].high - candles[index + 1].high;
+        var minusm = candles[index + 1].low - candles[index].low;
+
+        if (plusm > minusm) {}
+      },
+      max: (i, c, c2) {
+        return c2.map((e) => e.volume).toList().max();
+      },
+      min: (i, c, _) {
+        return 0;
+      }
+    // indicatorComponentsStyles: [
+    //   IndicatorStyle(name: "cci", bullColor: color),
+    // ]
+  );
 }
